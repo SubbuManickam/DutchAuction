f7b4a7ba334482c8a2af505e38d9c02f69f0bcce
Subbu Manickam <manickam.s@northeastern.edu>
2023-06-20 09:19:52 -0400

    v2.0

--- v1.0/test/BasicDutchAuction.ts
+++ v1.0/test/BasicDutchAuction.ts
@@ -23,38 +23,38 @@ describe('BasicDutchAuction', () => {
   it('Seller should not be allowed to place a bid', async () => {
     const bidAmount = ethers.parseEther('2.5');
 
-    await expect(auctionContract.connect(seller).bid({ value: bidAmount })).to.be.revertedWith('Sellers are not allowed to buy');
+    await expect(auctionContract!.connect(seller!).bid({ value: bidAmount })).to.be.revertedWith('Sellers are not allowed to buy');
   });
 
   it('Insufficient amount should not be accepted', async () => {
     const bidAmount = ethers.parseEther('1.5');
 
-    await expect(auctionContract.connect(bidder1).bid({ value: bidAmount })).to.be.revertedWith('Insufficient Amount');
+    await expect(auctionContract!.connect(bidder1!).bid({ value: bidAmount })).to.be.revertedWith('Insufficient Amount');
   });
 
   it('Choose the winner based on the conditions', async () => {
     const bidAmount = ethers.parseEther('2.5');
 
-    await auctionContract.connect(bidder1).bid({ value: bidAmount });
-    const winner = await auctionContract.winner();
+    await auctionContract!.connect(bidder1!).bid({ value: bidAmount });
+    const winner = await auctionContract!.winner();
 
-    expect(winner).to.equal(bidder1.address);
+    expect(winner).to.equal(bidder1!.address);
   });
 
   it('Conclude the auction after winner is chosen', async () => {
     const amount1 = ethers.parseEther('2.5');
     const amount2 = ethers.parseEther('2.5');
 
-    await auctionContract.connect(bidder1).bid({ value: amount1 });
-    await expect (auctionContract.connect(bidder2).bid({ value: amount2 })).to.be.revertedWith('Auction Concluded');
+    await auctionContract!.connect(bidder1!).bid({ value: amount1 });
+    await expect (auctionContract!.connect(bidder2!).bid({ value: amount2 })).to.be.revertedWith('Auction Concluded');
   });
 
   it('Transfer the bid amount to the seller when auction is over', async () => {
     const bidAmount = ethers.parseEther('2.5');
-    const initialSellerBalance = await ethers.provider.getBalance(seller.address);
+    const initialSellerBalance = await ethers.provider.getBalance(seller!.address);
 
-    await auctionContract.connect(bidder1).bid({ value: bidAmount });
-    const newSellerBalance = await ethers.provider.getBalance(seller.address);
+    await auctionContract!.connect(bidder1!).bid({ value: bidAmount });
+    const newSellerBalance = await ethers.provider.getBalance(seller!.address);
 
     const expectedBalance = initialSellerBalance + bidAmount;
     expect(newSellerBalance).to.equal(expectedBalance);
--- v2.0/contracts/NFTDutchAuction.sol
+++ v2.0/contracts/NFTDutchAuction.sol
@@ -2,9 +2,15 @@
 
 pragma solidity ^0.8.0;
 
-contract BasicDutchAuction {
+interface MintNFTokens{
+     function safeTransferFrom(address from, address to, uint256 tokenId) external;
+     function ownerOf(uint256 tokenId) external view returns(address owner);
+}
+
+contract NFTDutchAuction {
 
     address payable public seller;
+    address public currentOwner;
     address public buyer = address(0x0);
     address public winner = address(0x0);
 
@@ -13,21 +19,30 @@ contract BasicDutchAuction {
     uint256 immutable offerPriceDecrement;
     uint256 immutable initialPrice;
     uint256 curPrice;
-
+    
     uint256 immutable initialBlock;
     uint256 endBlock;
-    
+
+    uint256 immutable nfTokenId;
+    address immutable tokenAddress;
+    MintNFTokens mint;
+
     mapping(address => uint256) public bids;
 
-    constructor(uint256 _reservePrice, uint256 _numBlocksAuctionOpen, uint256 _offerPriceDecrement) {
+    constructor(address _tokenAddress, uint256 _nfTokenId, uint256 _reservePrice, uint256 _numBlocksAuctionOpen, uint256 _offerPriceDecrement) {
+        tokenAddress = _tokenAddress;
+        nfTokenId = _nfTokenId;
+        mint = MintNFTokens(tokenAddress);
         reservePrice = _reservePrice;
         numBlockAuctionOpen = _numBlocksAuctionOpen;
         offerPriceDecrement = _offerPriceDecrement;
         seller = payable(msg.sender);
+        currentOwner = seller;
         initialPrice = _reservePrice + (_numBlocksAuctionOpen * _offerPriceDecrement);
         initialBlock = block.number;
         endBlock = block.number + numBlockAuctionOpen;
         curPrice = initialPrice - ((block.number - initialBlock) * offerPriceDecrement);
+
     }
 
     function bid() public payable returns(address) {
@@ -37,10 +52,14 @@ contract BasicDutchAuction {
         bids[buyer] = msg.value;
         require(msg.value >= curPrice, "Insufficient Amount");
         require(winner == address(0x0), "Auction Concluded");
-        
+
         winner = msg.sender;
+        mint.safeTransferFrom(seller, winner, nfTokenId);
+
         seller.transfer(msg.value);
 
-        return winner;
+        currentOwner = winner;
+        return currentOwner;
     }
+
 }
